8cb83c03b3183a40ffc4a5c54737fa547be879e8
MrBigboka <e1984837@cmaisonneuve.qc.ca>
2022-02-02 09:42:42 -0500

    Initialisation du projet
    Voila

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+//import React, {useEffect, useState} from "react";
+import Typography from '@mui/material/Typography';
+//import {serveur} from "./constantes"
+
+const Home = () => {
+    return (
+      <div>
+          <Typography variant="h1"> FoodKeeper </Typography>
+      </div>
+    );
+}
+
+export default Home;
