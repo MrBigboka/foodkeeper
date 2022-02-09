dfc5172086340be7c39d630d2e450d2611d50f7a
Alfred <fredblade142@gmail.com>
2022-02-08 23:02:37 -0500

    Update Register.js

--- src/Register.js
+++ src/Register.js
@@ -121,6 +121,8 @@ export default function SignUp() {
                                 </Link>
                             </Grid>
                         </Grid>
+                        <br/>
+                        <br/>
                     </Box>
                 </Box>
             </Container>
