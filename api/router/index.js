e8f21b5f43a679142be5b05c14644b5f97fa0c5f
Alfred <fredblade142@gmail.com>
2022-03-01 00:06:29 -0500

    Update index.js

--- api/router/index.js
+++ api/router/index.js
@@ -3,6 +3,9 @@ const authMiddleware = require('../modules/auth-middleware');
 const restaurants = require('./restaurants');
 const auth = require('./auth');
 const profile = require('./profile');
+const images = require('./images');
+const reservation = require('./reservations');
+const router = Router();
 
 router.use('/auth', auth);
 router.use('/restaurants', restaurants);
